55cf1cce816f45b3627cecb46df17dcf40ee6035
kiefk <kiefk@oregonstate.edu>
2017-03-02 20:46:46 -0800

    added military and civilian child classes

---
+++ src/main/java/edu/oregonstate/cs361/battleship/Civilian.java
@@ -0,0 +1,10 @@
+package edu.oregonstate.cs361.battleship;
+
+/**
+ * Created by Keana on 3/2/2017.
+ */
+public class Civilian extends Ship {
+
+
+
+}
---
+++ src/main/java/edu/oregonstate/cs361/battleship/Military.java
@@ -0,0 +1,8 @@
+package edu.oregonstate.cs361.battleship;
+
+/**
+ * Created by Keana on 3/2/2017.
+ */
+public class Military extends Ship {
+
+}
